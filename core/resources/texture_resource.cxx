d47ee03cb9813f718ce8d61736b3135c05664cb7
oraqlle <oraqlle@proton.me>
2026-03-18 10:55:46 +1100

    wip(core): Initial implementation of Texture resource
    Currently uses dynamic polymorphism to handle Resource to Texture
    hierarchy but will move to making it use Curiously Recurring Template
    Pattern (CRTP) for static polymorphism instead.

---
+++ core/resources/texture_resource.cxx
@@ -0,0 +1,78 @@
+// <core/resources/texture_resource.cxx> -*- C++ -*-
+
+//  Sapling 3D Game Engine
+//  Copyright (C) 2026  Tyler Swann, Georgia Kannelis
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License v2.1 as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+//  USA
+
+#include <texture_resource.h>
+
+#include <format>
+
+namespace sap::core::resources {
+
+Texture::Texture(const std::string& id)
+    : Resource(id) {}
+
+Texture::~Texture() { unload(); }
+
+auto Texture::get_image() const -> vk::Image { return m_image; }
+
+auto Texture::get_image_view() const -> vk::ImageView { return m_image_view; }
+
+auto Texture::get_sampler() const -> vk::Sampler { return m_sampler; }
+
+auto Texture::do_load() -> bool {
+    auto file_path = std::format("textures/{}.ktx", get_id());
+
+    auto data = _M_load_image_data(file_path);
+
+    if (data.empty()) {
+        return false;
+    }
+
+    _M_create_vulkan_image(data);
+    _M_free_image_data(data);
+
+    return true;
+}
+
+auto Texture::do_unload() -> bool {
+    if (is_loaded()) {
+        auto device = _M_get_device();
+
+        device.destroySampler(m_sampler);
+        device.destroyImageView(m_image_view);
+        device.destroyImage(m_image);
+        device.freeMemory(m_memory);
+
+        return true;
+    }
+
+    return false;
+}
+
+auto Texture::_M_load_image_data(const std::string& file_path)
+    -> const std::vector<std::byte> {
+    return std::vector<std::byte>{};
+}
+
+auto Texture::_M_free_image_data(const std::vector<std::byte> data) -> void {}
+
+auto Texture::_M_create_vulkan_image(const std::vector<std::byte> data) -> void {}
+
+auto Texture::_M_get_device() const -> vk::Device { return vk::Device(); }
+
+} // namespace sap::core::resources
---
+++ core/resources/texture_resource.h
@@ -0,0 +1,76 @@
+// <core/resources/texture_resource.h> -*- C++ -*-
+
+//  Sapling 3D Game Engine
+//  Copyright (C) 2026  Tyler Swann, Georgia Kannelis
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License v2.1 as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+//  USA
+
+#ifndef SAPLING_ENGINE_TEXTURE_RESOURCE_H
+#define SAPLING_ENGINE_TEXTURE_RESOURCE_H
+
+#include <base_resource.h>
+#include <resource_handle.h>
+#include <resource_manager.h>
+
+#include "vulkan/vulkan.hpp"
+
+#include <cstddef>
+#include <span>
+
+namespace sap::core::resources {
+
+class Texture : public Resource {
+private:
+    // Vulkan GPU objects
+    vk::Image m_image;          // GPU object containing pixel data
+    vk::DeviceMemory m_memory;  // GPU memory storing image
+    vk::DeviceSize m_offset;    // Offset within memory allocation containing image
+    vk::ImageView m_image_view; // Shader-accessible view of image
+    vk::Sampler m_sampler;      // Sampling config for filtering, wrapping etc.
+
+    // Texture metadata for validation and debugging
+    size_t m_width = 0;    // Image width in pixels
+    size_t m_height = 0;   // Image height in pixels
+    size_t m_channels = 0; // Number of colour channels (RGB=3, RGBA=4)
+
+public:
+    explicit Texture(const std::string& id);
+
+    ~Texture() override;
+
+    auto get_image() const -> vk::Image;
+
+    auto get_image_view() const -> vk::ImageView;
+
+    auto get_sampler() const -> vk::Sampler;
+
+protected:
+    bool do_load() override;
+    bool do_unload() override;
+
+private:
+    auto _M_load_image_data(const std::string& file_path) -> const std::vector<std::byte>;
+
+    auto _M_free_image_data(const std::vector<std::byte> data) -> void;
+
+    auto _M_create_vulkan_image(const std::vector<std::byte> data) -> void;
+
+    auto _M_get_device() const -> vk::Device;
+
+}; // class Texture
+
+} // namespace sap::core::resources
+
+#endif // SAPLING_ENGINE_TEXTURE_RESOURCE_H
