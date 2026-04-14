f6018857d1cb7c387a96796bac3a3c8e6dc7f458
oraqlle <oraqlle@proton.me>
2026-04-15 08:10:56 +1000

    wip(core): Scene culling system

---
+++ core/renderer/culling_system.cxx
@@ -0,0 +1,35 @@
+// <core/resources/scene_culling.cxx> -*- C++ -*-
+
+//  Sapling 3D Game Engine
+//  Copyright (C) 2026  Tyler Swann, Georgia Kannelis
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License v2.1 as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+//  USA
+
+#include "scene_culling.h"
+
+namespace sap::core::resources {
+
+auto Resource::get_id() const -> const std::string& { return m_resource_id; }
+
+auto Resource::is_loaded() const -> bool { return loaded; }
+
+auto Resource::load() -> bool {
+    loaded = do_load();
+    return loaded;
+}
+
+auto Resource::unload() -> void { loaded = do_unload(); }
+
+} // namespace sap::core::resources
---
+++ core/renderer/culling_system.h
@@ -0,0 +1,45 @@
+// <core/resources/culling_system.h> -*- C++ -*-
+
+//  Sapling 3D Game Engine
+//  Copyright (C) 2026  Tyler Swann, Georgia Kannelis
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License v2.1 as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+//  USA
+
+#ifndef SAPLING_ENGINE_CULLING_SYSTEM_H
+#define SAPLING_ENGINE_CULLING_SYSTEM_H
+
+#include <vector>
+
+namespace sap::core::renderer {
+
+class CullingSystem {
+private:
+    Camera *m_camera;
+    std::vector<Entity *> m_visible_entities;
+
+public:
+    explicit CullingSystem(Camera *camera);
+
+    auto change_camera(Camera *to_camera) -> void;
+
+    auto cull_scene(const std::vector<Entity *>& entities) -> void;
+
+    auto visable_entities() const -> const std::vector<Entity *>&;
+
+}; // class CullingSystem
+
+} // namespace sap::core::renderer
+
+#endif // SAPLING_ENGINE_CULLING_SYSTEM_H
